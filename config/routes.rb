c9faee28906390ccdd8453724f81c6c91474cbb7
Steve <stevenroger@comcast.net>
2012-11-09 20:49:52 -0800

    Setup devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,19 @@
 Coderconnection::Application.routes.draw do
+  #get "users/index"
+
+  #get "users/show"
+
+  #get "home/index"
+
+  authenticated :user do
+    root :to => 'home#index'
+  end
+  
+  root :to => "home#index"
+  
+  devise_for :users
+  resources :users
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
