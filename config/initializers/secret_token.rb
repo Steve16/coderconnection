893f6183821b14eda362a5ada37abd7da6e1da38
Steve <stevenroger@comcast.net>
2012-11-09 18:26:00 -0800

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Coderconnection::Application.config.secret_token = '89b98b5b6be482f5e54acbebe31a8ceaf1ff2cdc4624cf4e76869f5c546b4d9c2b29b3e4533131548060cd8da54e56dbfe56fe42b16e4e59025ed31d1c2641b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Coderconnection::Application.config.session_store :cookie_store, key: '_coderconnection_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Coderconnection::Application.config.session_store :active_record_store
